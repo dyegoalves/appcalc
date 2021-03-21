0da2c1d28143b17991735b5981430570c3d74348
Dyego Alves <dyego.santos.alves@gmail.com>
2021-03-21 05:50:28 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Principal App cs GO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+p.ft {
+  font-size: 10px;
+}
+</style>
