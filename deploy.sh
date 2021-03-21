c411d91505bc113a532356e2de390147d14abec2
Dyego Alves <dyego.santos.alves@gmail.com>
2021-03-21 05:54:05 -0300

    Correction the Deploy gh-pages config

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:csgoinvcalc/app.git master:gh-pages
+git push -f git@github.com:csgoinvcalc/appcalc.git master:gh-pages
 
 cd -
